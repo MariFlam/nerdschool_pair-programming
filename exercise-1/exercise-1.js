c4f01e8a11100b6320776e6ff3fced774059c1bd
Mari Flåm <tabasco_world@hotmail.com>
2023-02-27 21:10:56 +0100

    add solution to fizzbuzz and tests to go wih it

--- exercise-1/exercise-1.js
+++ exercise-1/exercise-1.js
@@ -1,6 +1,32 @@
 
 function fizzBuzzRange(max) {
-  return [];
+  const array = []
+  for (let count = 1; count <= max; count = count + 1) {
+    let isFizBuzz = ""
+    if (isDivisibleByThree(count)) {
+      isFizBuzz = "Fizz"
+    }
+    if (isDivisibleByFive(count)) {
+      isFizBuzz = isFizBuzz + "Buzz"
+    }
+    if (isFizBuzz) {
+      array.push(isFizBuzz)
+    } else {
+      array.push(count)
+    }
+
+  }
+
+  return array;
 }
+
+function isDivisibleByThree(num) {
+  return (num % 3) === 0
+}
+
+function isDivisibleByFive(num) {
+  return (num % 5) === 0
+}
+
 
 module.exports.fizzBuzzRange = fizzBuzzRange;
--- exercise-1/exercise-1.test.js
+++ exercise-1/exercise-1.test.js
@@ -1,15 +1,17 @@
 const exercise1 = require("./exercise-1");
 
+
 describe("fizzBuzzRange", () => {
 
-  test("One should equal one", () => {
-    expect(1).toBe(1);
+  test("verify that the length of returned array is equal to max", () => {
+    const result = exercise1.fizzBuzzRange(5)
+    expect(result.length).toBe(5);
   });
 
   test("Array content should be equal", () => {
     expect([1, 2, "hello"]).toEqual([1, 2, "hello"]);
   })
-  
+
   test("please name me as well", () => {
     const correctResult = [
       1,
@@ -33,7 +35,9 @@ describe("fizzBuzzRange", () => {
       19,
       "Buzz"
     ];
-
+    const result = exercise1.fizzBuzzRange(20)
+    expect(result).toEqual(correctResult);
     // Some test calling exercise1.fizzBuzzRange(someinput)
+
   });
 });
